9c15785faead7f3dbabc79862760a0b28f9d17cd
tduong77 <131144063+tduong77@users.noreply.github.com>
2023-08-04 15:11:30 +0700

    version 1

---
+++ components/Header.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+  const handleLogout = () => {
+    //
+  };
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            className="flex rounded-full bg-black items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.back()}
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button
+            className="flex rounded-full bg-black items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4 basis-1/6">
+          <Button
+            className="bg-white basis-1/2"
+            onClick={() => {
+              console.log("log in");
+            }}
+          >
+            Log in
+          </Button>
+          <Button
+            className="bg-transparent text-neutral-300 font-medium basis-1/2"
+            onClick={() => {
+              console.log("sign up");
+            }}
+          >
+            Sign up
+          </Button>
+        </div>
+      </div>
+        {children}
+    </div>
+  );
+};
+export default Header;
